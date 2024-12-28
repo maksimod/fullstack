1d2dd57f7bfe9a7116eb1155d547fc89406f03e1
maksimod <maksumonka@gmail.com>
2024-12-28 21:59:20 +0300

    some trash2

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ViteXd1 + gReactxed</h1>
+      <h1>ViteXd12 + gReactxed</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
