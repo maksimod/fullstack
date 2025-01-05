ad99a91cac8ed9099646d8883abfe9eef765ee0c
maksimod <maksumonka@gmail.com>
2025-01-05 16:54:45 +0300

    test

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,22 +1,22 @@
 export const App = () => {
   const ideas = [
-    { id: 'cool-idea-nick-1', name: 'idea1', description: 'Description of idea 1' },
-    { id: 'cool-idea-nick-2', name: 'idea2', description: 'Description of idea 2' },
-    { id: 'cool-idea-nick-3', name: 'idea3', description: 'Description of idea 3' },
-    { id: 'cool-idea-nick-4', name: 'idea4', description: 'Description of idea 4' },
-    { id: 'cool-idea-nick-5', name: 'idea5', description: 'Description of idea 5' },
+    { nick: "cool-idea-nick-1", name: 'idea1', description: 'Description of idea 1' },
+    { nick: "cool-idea-nick-2", name: 'idea2', description: 'Description of idea 2' },
+    { nick: "cool-idea-nick-3", name: 'idea3', description: 'Description of idea 3' },
+    { nick: "cool-idea-nick-4", name: 'idea4', description: 'Description of idea 4' },
+    { nick: "cool-idea-nick-5", name: 'idea5', description: 'Description of idea 5' },
   ]
   return (
     <div>
       <h1>My Ideas</h1>
-      {ideas.map((idea) => {
-        return (
-          <div key={idea.id}>
-            <h2>{idea.name}</h2>
-            <p>{idea.description}</p>
-          </div>
-        )
-      })}
+        {ideas.map((idea)=> {
+          return (
+            <div key={idea.nick}>
+              <h2>{idea.name}</h2>
+              <p>{idea.description}</p>
+            </div>
+          )
+        })}
     </div>
   )
 }
