4254985d2f0048975cdb806e9e949f676348a0e9
maksimod <maksumonka@gmail.com>
2025-01-09 13:04:18 +0300

    Add my husky

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -8,12 +8,6 @@ const ideas = [
   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
 ]
 
-const x: string = 'hello'
-
-if (Math.random() + 1) {
-  console.info(x)
-}
-
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
