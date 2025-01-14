ecc9ca94309f9e7f94f4d9c363b0a38fc79fb7df
maksimod <maksumonka@gmail.com>
2025-01-14 15:23:32 +0300

    22_try

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,16 +1,11 @@
 import { useParams } from 'react-router-dom'
+import { type ViewIdeaRouteParams } from '../../lib/routes'
 
 export const ViewIdeaPage = () => {
-  const { ideaNick } = useParams() as { ideaNick: string }
+  const { ideaNick } = useParams() as ViewIdeaRouteParams
   return (
     <div>
       <h1>{ideaNick}</h1>
-      <p>Description of idea 1...</p>
-      <div>
-        <p>Text paragrph 1 of idea 1...</p>
-        <p>Text paragrph 2 of idea 1...</p>
-        <p>Text paragrph 3 of idea 1...</p>
-      </div>
     </div>
   )
 }
