bdf685f0459928aab7efe6e7e73c82b71e57cce2
maksimod <maksumonka@gmail.com>
2025-01-14 15:49:23 +0300

    23_stable

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -6,6 +6,12 @@ export const ViewIdeaPage = () => {
   return (
     <div>
       <h1>{ideaNick}</h1>
+      <p>Description of idea 1</p>
+      <div>
+        <p>Text of par1 idea 1</p>
+        <p>Text of par2 idea 1</p>
+        <p>Text of par3 idea 1</p>
+      </div>
     </div>
   )
 }
