eb262c40a119b780aa64e4d3848a34d236cb92e4
Maksimod <maksumonka@gmail.com>
2025-01-31 22:03:59 +0300

    63_done

--- webapp/src/pages/SignInPage/index.tsx
+++ webapp/src/pages/SignInPage/index.tsx
@@ -1,15 +1,22 @@
 import { zSignInTrpcInput } from '@vscode_frontend/backend/src/router/signIn/input'
 import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
+import Cookies from 'js-cookie'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Alert } from '../../components/Alert'
 import { Button } from '../../components/Button'
 import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { getAllIdeasRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 
 export const SignInPage = () => {
+  const navigate = useNavigate()
+  const trpcUtils = trpc.useContext()
+  // @ts-expect-error unused state variables will be used later
+  // eslint-disable-next-line
   const [successMessageVisible, setSuccessMessageVisible] = useState(false)
   const [submittingError, setSubmittingError] = useState<string | null>(null)
   const signIn = trpc.signIn.useMutation()
@@ -22,12 +29,11 @@ export const SignInPage = () => {
     onSubmit: async (values) => {
       try {
         setSubmittingError(null)
-        await signIn.mutateAsync(values)
-        formik.resetForm()
-        setSuccessMessageVisible(true)
-        setTimeout(() => {
-          setSuccessMessageVisible(false)
-        }, 3000)
+        const { token } = await signIn.mutateAsync(values)
+        Cookies.set('token', token, { expires: 99999 })
+        void trpcUtils.invalidate()
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        navigate(getAllIdeasRoute())
       } catch (err: any) {
         setSubmittingError(err.message)
       }
@@ -42,7 +48,6 @@ export const SignInPage = () => {
           <Input label="Password" name="password" type="password" formik={formik} />
           {!formik.isValid && !!formik.submitCount && <Alert color="red">Some fields are invalid</Alert>}
           {submittingError && <Alert color="red">{submittingError}</Alert>}
-          {successMessageVisible && <Alert color="green">Thanks for sign in!</Alert>}
           <Button loading={formik.isSubmitting}>Sign In</Button>
         </FormItems>
       </form>
---
+++ webapp/src/pages/SignOutPage/index.tsx
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getSignInRoute } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
+
+export const SignOutPage = () => {
+  const navigate = useNavigate()
+  const trpcUtils = trpc.useContext()
+  useEffect(() => {
+    Cookies.remove('token')
+    void trpcUtils.invalidate().then(() => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      navigate(getSignInRoute())
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <p>Loading...</p>
+}
