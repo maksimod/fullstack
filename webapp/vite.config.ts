48a7ac0b6e5ea04a4afa487d523d560ea8133d92
Maksimod <maksumonka@gmail.com>
2025-02-01 12:41:05 +0300

    65_done

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: +env.PORT,
     },
-  },
+    preview: {
+      port: +env.PORT,
+    },
+  }
 })
