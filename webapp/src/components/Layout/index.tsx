b4de004aca97b844097a23a822c9dad45b22598d
Maksimod <maksumonka@gmail.com>
2025-02-03 16:28:15 +0300

    71_done

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import { Link, Outlet } from 'react-router-dom'
+import { useMe } from '../../lib/ctx'
 import { getAllIdeasRoute, getNewIdeaRoute, getSignInRoute, getSignOutRoute, getSignUpRoute } from '../../lib/routes'
-import { trpc } from '../../lib/trpc'
 import css from './index.module.scss'
 
 // console.log(css)
 
 export const Layout = () => {
-  const { data, isLoading, isFetching, isError } = trpc.getMe.useQuery()
+  const me = useMe()
 
   return (
     <div className={css.layout}>
@@ -18,7 +18,7 @@ export const Layout = () => {
               All Ideas
             </Link>
           </li>
-          {isLoading || isFetching || isError ? null : data.me ? (
+          {me ? (
             <>
               <li className={css.item}>
                 <Link className={css.link} to={getNewIdeaRoute()}>
@@ -27,7 +27,7 @@ export const Layout = () => {
               </li>
               <li className={css.item}>
                 <Link className={css.link} to={getSignOutRoute()}>
-                  Log Out ({data.me.nick})
+                  Log Out ({me.nick})
                 </Link>
               </li>
             </>
