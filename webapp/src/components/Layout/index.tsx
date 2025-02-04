4a9fc46ffb1cd81b5235257f18ab7950b7b0d595
Maksimod <maksumonka@gmail.com>
2025-02-04 15:59:54 +0300

    76_done

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,6 +1,13 @@
 import { Link, Outlet } from 'react-router-dom'
 import { useMe } from '../../lib/ctx'
-import { getAllIdeasRoute, getNewIdeaRoute, getSignInRoute, getSignOutRoute, getSignUpRoute } from '../../lib/routes'
+import {
+  getAllIdeasRoute,
+  getEditProfileRoute,
+  getNewIdeaRoute,
+  getSignInRoute,
+  getSignOutRoute,
+  getSignUpRoute,
+} from '../../lib/routes'
 import css from './index.module.scss'
 
 // console.log(css)
@@ -23,6 +30,11 @@ export const Layout = () => {
               <li className={css.item}>
                 <Link className={css.link} to={getNewIdeaRoute()}>
                   Add Idea
+                </Link>
+              </li>
+              <li className={css.item}>
+                <Link className={css.link} to={getEditProfileRoute()}>
+                  Edit Profile
                 </Link>
               </li>
               <li className={css.item}>
