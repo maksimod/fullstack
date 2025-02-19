c84f524cd54487c0c4510da6494292efe5430d7d
Maksimod <maksumonka@gmail.com>
2025-02-19 20:12:05 +0300

    108_done

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,24 +1,37 @@
+import { EOL } from 'os'
 import { serializeError } from 'serialize-error'
 import winston from 'winston'
+import * as yaml from 'yaml'
 import { env } from './env'
 
+// Настройка цветов для уровней логирования
+const customColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'cyan',
+  debug: 'green',
+}
+
+// Добавляем цвета в Winston
+winston.addColors(customColors)
+
+// Создаем Winston Logger
 export const winstonLogger = winston.createLogger({
   level: 'debug',
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.errors({ stack: true }),
-    winston.format.json(),
+    winston.format.colorize({ all: true }), // Включаем цветизацию для всех полей
+    winston.format.printf(({ level, message, timestamp, ...meta }) => {
+      const metaString = Object.keys(meta).length ? `${EOL}${yaml.stringify(meta)}` : ''
+      return `${timestamp} ${level}: ${message}${metaString}`
+    }),
   ),
   defaultMeta: { service: 'backend', hostEnv: env.HOST_ENV },
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.json(),
-    }),
-  ],
+  transports: [new winston.transports.Console()],
 })
 
+// Обертка для удобного использования логгера
 export const logger = {
   info: (logType: string, message: string, meta?: Record<string, any>) => {
     winstonLogger.info(message, { logType, ...meta })
